338bcbc5ccd29d759c3a869c43b24aa854355577
Artiom <artiombotoroga@gmail.com>
2016-05-17 06:44:37 +0400

    adaug lab#6

---
+++ lab#6/app/src/main/java/anna/howtocelebratechristmas/Beverages_Data.java
@@ -0,0 +1,349 @@
+package anna.howtocelebratechristmas;
+
+import android.graphics.Color;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Beverages_Data {
+
+    private Beverages_Data() {
+    }
+
+    public static List<Information> load_beverages_items() {
+        List<Information> items = new ArrayList<>();
+
+
+        Information beverages_information_2 = new Information();
+        beverages_information_2.name = "E usor sa faci lichor cu ou batut. ";
+        beverages_information_2.description = "Lichor cu ou batut este o bautura foarte delicioasa. " +
+                "Eu nu exageres cind spun ca e mai ok decit cel cumparat din magazin!  \n" +
+                "In unele magazine lichior cu ou baut este prea mult gros si cu mult zahar dulce. " +
+                "Acest lucru de casa este incredib de proaspat si cremos si dureaza doar citeva minute pentru a o face. \n" +
+                "Lichior cu oua batute se face cu oua crude,care dau un gust bogat si delicios. \n" +
+                "In aceasta receta,se foloseste oua pasteurizate gasite la magazin local.\n\n" +
+                "Ingridiente\n" +
+                "\n" +
+                "    4 galbenusuri de ou, din ouale cele pasteurizate\n" +
+                "    1/3 pahar de zahar\n" +
+                "    2 pahare pline de lapte\n" +
+                "    1 pahar greu de crema\n" +
+                "    1/2 de extract de vanilie pura\n" +
+                "    1 1/2 lingurite de nucusoare prospat ras \n" +
+                "\n" +
+                "Instructii\n" +
+                "\n" +
+                "    In castron pentru mixter, se amesteca galbenusurile de ou la viteza medie.\n" +
+                "    Se adauga incet zaharul doar pina cind sunt bine combinate, ouale trebuie sa fie de culoare - galben deschis .\n" +
+                "    Razuiti partile castronului asa cum este necesar.\n" +
+                "    La rindul sau, se aduga lapte,smintina,vanilie si nucushoare.\n" +
+                "    Trebuie de a servi rece. \n" +
+                "\n" +
+                "Notite\n" +
+                "\n" +
+                "Utilizati o intreaga nucusoare, proaspat, pentru al rade de a supra gustarii delicioase. ";
+        beverages_information_2.color = Color.rgb(111, 11, 111);
+        beverages_information_2.image = "Beverage";
+        beverages_information_2.URL = "https://www.youtube.com/watch?v=RWIYCGVTNLM";
+
+        items.add(beverages_information_2);
+
+
+        Information beverages_information_1 = new Information();
+        beverages_information_1.name = "Craciunul Betty's Wassail ";
+        beverages_information_1.description = "Craciunul Betty acush demonstreza cum se sarbatoreste  " +
+                "Wassail este pentru soarbatoarea vastra de craciun.\n" +
+                "Este o bautura traditionala de craciun. " +
+                "Citeva mere,suc de afine,unele condimente bautura alcoolica ROM desigur daca se doreste.\n" +
+                "\n" +
+                "Craciunul Wassail\n" +
+                "\n" +
+                "\t½  dintr-un mar\n" +
+                "\t1 o halba de suc de afine\n" +
+                "\t¾ dintr-un pahar de zahar\n" +
+                "\t1 o lingurita de ienibahar,\n" +
+                "\t1 o lingurita de arahide intregi\n" +
+                "\t2 scortishoare bastonase\n" +
+                "\t1 putin ROM\n" +
+                "\n" +
+                "Punem toate ingrietele la aragaz lent,se duce la fierbere mare, apoi se reduce focul la mic si se fierbe pina cind este gata de a servi, aproximativ 3 ore,si apoi salut Betty---♥♥♥";
+        beverages_information_1.color = Color.rgb(240, 280, 255);
+        beverages_information_1.image = "Beverage_2";
+        beverages_information_1.URL = "https://www.youtube.com/watch?v=lb_zLZVrvpo";
+
+        items.add(beverages_information_1);
+
+
+
+
+        Information beverages_information_3 = new Information();
+        beverages_information_3.name = "French Ciocolata fierbinte ";
+        beverages_information_3.description = "Cele mai decante recete ale ciocolatei fierbiti. " +
+                "are un gust la fel ca ciocolata fierbite franceza gasita in cafenelele din paris," +
+                " unde cerul e bogat absolut pentru orice iubitor de ciocolata. " +
+                "Receta a celebrei cafenelei Angelina din Paris.\n\n" +
+                "Randament: 2 cupe mari intense de ciocolata calda sau 4 cesti mai rezonabile la dimensiuni\n\n" +
+                "Ingridiente:\n" +
+                "\n" +
+                "    1 1/2 pahar de lapte\n" +
+                "    1/2 pahar de crema grea\n" +
+                "    2 linguriti de zahar\n" +
+                "    1/2 lingura de bucatarie de espresso curat\n" +
+                "    8 bucati de ciocolata amaruie , cel putin 70% tocat\n" +
+                "    Un castron uriash de frisca pentru servire\n" +
+                "\n" +
+                "Directii:\n" +
+                "\n" +
+                "    Intr-o cratita medie de foc mediu,amestecam laptele integral, " +
+                "daca apare crema spumoasa,adaugam zahar si espresso pina se face intro stare de bule mari " +
+                "si nu se lasa amestecul sa fiarba.\n" +
+                "    Se scoate din cratitza de pe foc si se amesteca in ciocolata tocata pina la " +
+                "topit,revenind sosul la foc mic este necesar pentru a topi ciocolata " +
+                "complet,si apoi adaugam crema desa si grea.\n" +
+                "\n";
+        beverages_information_3.color = Color.rgb(13, 55, 125);
+        beverages_information_3.image = "Beverage_3";
+        beverages_information_3.URL = "https://www.youtube.com/watch?v=kquPQiA7BdA";
+
+        items.add(beverages_information_3);
+
+
+
+
+        Information beverages_information_4 = new Information();
+        beverages_information_4.name = "Irish Cream Liqueur ";
+        beverages_information_4.description = "You’ll love this easy to make homemade Irish Cream Recipe.\n" +
+                "\n" +
+                "Ingredients\n" +
+                "\n" +
+                "    1¼ cup heavy cream\n" +
+                "    1 can sweetened condensed milk (14 oz)\n" +
+                "    1⅔ Cup Irish Whiskey\n" +
+                "    2 teaspoons instant coffee granules\n" +
+                "    2 tablespoons chocolate syrup\n" +
+                "    1 teaspoon almond extract\n" +
+                "    1 teaspoon vanilla extract\n" +
+                "\n" +
+                "Instructions\n" +
+                "\n" +
+                "    Using a blender, combine all ingredients and blend on high for approximately 30 seconds. Pour into seal-able bottle or sealed container. Store in refrigerator up to 2 weeks. Shake well before serving.\n\n";
+        beverages_information_4.color = Color.rgb(13, 55, 125);
+        beverages_information_4.image = "beverage_4";
+        beverages_information_4.URL = "http://todayscreativelife.com/irish-cream-recipe/";
+
+        items.add(beverages_information_4);
+
+
+
+
+
+        Information beverages_information_5 = new Information();
+        beverages_information_5.name = "Holiday Sparkler ";
+        beverages_information_5.description = "Take a break from mimosas on New Year's Day, " +
+                "try this pretty, pink sparkler. Cranberry, lime and mint dance in the " +
+                "punch bowl with effervescent cava, making the drink tart, sweet, verdant and bright. " +
+                "Cava leads, though, so be careful–this stuff goes straight to the brain.\n\n" +
+                " Makes 1 punch bowl (or 25 single drinks)\n" +
+                "\n" +
+                "    4 bottles cava (chilled)\n" +
+                "    2 bunches fresh mint\n" +
+                "    1 cup triple sec\n" +
+                "    1 cup freshly squeezed lime juice\n" +
+                "    1 cup cranberry juice\n" +
+                "    additional mint, lime or fresh cranberries for garnish\n" +
+                "\n" +
+                "    The night or morning before your party mix together " +
+                "the triple sec, lime juice, and cranberry. Mash up (muddle)" +
+                " the mint and toss into the liquids. Let sit overnight and remove the mint.\n" +
+                "    Add your cava to punch bowl first. Then add about 3/4 " +
+                "of your mixer (triple sec, lime, and cranberry). Taste -" +
+                " everyone has a preference on sweetness so you can stop here " +
+                "or add the rest. Serve chilled.\n" +
+                "    If you wanted to serve a la carte rather than in a punch bowl, " +
+                "add about 1 oz of the mixer to the bottom of a champagne flute then" +
+                " fill with cava. Enjoy!!!\n" +
+                "\n";
+        beverages_information_5.color = Color.rgb(222, 22, 222);
+        beverages_information_5.image = "beverage_5";
+        beverages_information_5.URL = "http://food52.com/recipes/14968-holiday-sparkler";
+        items.add(beverages_information_5);
+
+
+        Information beverages_information_6 = new Information();
+        beverages_information_6.name = "Peppermint White Hot Chocolate ";
+        beverages_information_6.description = "This peppermint white hot chocolate recipe is " +
+                "simple to make, and perfectly sweetened with peppermint.\n\n" +
+                "Ingredients:\n" +
+                "\n" +
+                "    4 cups of milk of your choice\n" +
+                "    8 oz. white chocolate, chopped into small pieces (or white chocolate chips)\n" +
+                "    1 teaspoon vanilla extract\n" +
+                "    1/2 teaspoon peppermint extract (or more/less to taste)\n" +
+                "    optional toppings: whipped cream, crushed peppermints, marshmallows\n" +
+                "\n" +
+                "Directions:\n" +
+                "\n" +
+                "Stir the milk and chopped white chocolate together in a medium saucepan.  Cook over medium-low heat, stirring occasionally, until the mixture comes to a simmer and the chocolate is melted.  (Do not let it come to a boil or let the chocolate burn on the bottom.)  Remove from heat and stir in vanilla and peppermint extract.\n" +
+                "\n" +
+                "Serve warm, with optional toppings if desired.\n\n";
+        beverages_information_6.color = Color.rgb(222, 122, 222);
+        beverages_information_6.image = "beverage_6";
+        beverages_information_6.URL = "http://www.gimmesomeoven.com/peppermint-white-hot-chocolate-recipe/";
+        items.add(beverages_information_6);
+
+
+
+
+
+
+        Information beverages_information_7 = new Information();
+        beverages_information_7.name = "Coconut Milk Eggnog ";
+        beverages_information_7.description = "This dairy-free version of the classic Christmas drink uses creamy, thick, and rice coconut milk.\n" +
+                "\n" +
+                "Ingredients\n" +
+                "\n" +
+                "    1-14 ounce can full fat coconut milk\n" +
+                "    2 cups light coconut milk (either from the can or a carton)\n" +
+                "    4 egg yolks\n" +
+                "    1/2 cup maple syrup\n" +
+                "    1 teaspoon ground cinnamon\n" +
+                "    1 teaspoon ground nutmeg\n" +
+                "    1/8 teaspoon ground allspice\n" +
+                "    1 teaspoon vanilla extract\n" +
+                "    Extra nutmeg for serving\n" +
+                "\n" +
+                "Instructions\n" +
+                "\n" +
+                "    Combine all ingredients in the basin of a blender. Blend on high until well-combined and frothy. Chill until cold, then serve topped with nutmeg (and maybe with a splash of rum or bourbon for the adults)." +
+                "\nYield: 8 servings\n" +
+                "\n\n";
+        beverages_information_7.color = Color.rgb(222, 122, 222);
+        beverages_information_7.image = "beverage_7";
+        beverages_information_7.URL = "http://backtoherroots.com/2014/11/25/coconut-milk-eggnog/";
+        items.add(beverages_information_7);
+
+
+
+
+
+        Information beverages_information_8 = new Information();
+        beverages_information_8.name = "Traditional Winter Wassail ";
+        beverages_information_8.description = "Ingredients\n" +
+                "\n" +
+                "    7 – 8 cups organic apple cider\n" +
+                "    2 cups organic cranberry juice\n" +
+                "    10 whole cloves\n" +
+                "    10 allspice berries\n" +
+                "    2 cinnamon sticks\n" +
+                "    1 organic orange, sliced into rounds (if organic is unavailable, just use a conventional one BUT you will want to peel it first.)\n" +
+                "    *Optional: 1 cup dark ale or wine of your choosing, OR ½ cup rum or brandy in the pot. If you don’t want the whole pot spiked, you can do splashes of the alcohol in each mug. Since I serve this to my girls too I just add wine to the mug that my husband and I drink from.\n" +
+                "    *Optional: organic honey to sweeten if you desire – I usually don’t, but it is good!\n" +
+                "\n" +
+                "Instructions\n" +
+                "\n" +
+                "    Tie up the cloves and allspice in a cheesecloth or small piece of tea towel using kitchen twine.\n" +
+                "    Put everything into a large soup or stock pot and bring to a low simmer.\n" +
+                "    Lower the heat and very low simmer the mixture for 2 hours before serving. Keep it on low or use a crockpot on low if serving over hours.\n\n" +
+                "\n\n";
+        beverages_information_8.color = Color.rgb(222, 122, 222);
+        beverages_information_8.image = "beverage_8";
+        beverages_information_8.URL = "http://www.raisinggenerationnourished.com/2014/12/winter-wassail/";
+        items.add(beverages_information_8);
+
+
+
+
+        Information beverages_information_9 = new Information();
+        beverages_information_9.name = "Mint White Hot Chocolate ";
+        beverages_information_9.description = "Homemade Mint White Hot Chocolate combines creamy, " +
+                "white chocolate with a burst of peppermint flavor to create a perfect decadent holiday treat.\n\n" +
+                "Ingredients\n" +
+                "\n" +
+                "    2 cups milk\n" +
+                "    2 cups half & half\n" +
+                "    1 bag white chocolate chips\n" +
+                "    1 teaspoon peppermint extract\n" +
+                "    ¼ teaspoon vanilla extract\n" +
+                "    Optional: A few drops of food coloring\n" +
+                "    Marshmallows for topping\n" +
+                "\n" +
+                "Instructions\n" +
+                "\n" +
+                "    In a large pot, add milk and half-and-half, and heat over medium heat until steam-y but not boiling.\n" +
+                "    Stir in the white chocolate chips. Continue stirring until chocolate is completely melted and the mixture is creamy.\n" +
+                "    Stir in food coloring and extracts. Stir until smooth. Serve warm.\n" +
+                "\t\n" +
+                "Serves: 4-5\n" +
+                "\n" +
+                "\n\n";
+        beverages_information_9.color = Color.rgb(222, 122, 222);
+        beverages_information_9.image = "beverage_9";
+        beverages_information_9.URL = "http://www.thegraciouswife.com/homemade-mint-white-hot-chocolate/";
+        items.add(beverages_information_9);
+
+
+
+        Information beverages_information_10 = new Information();
+        beverages_information_10.name = "Spiked Slow Cooker Caramel Apple Cider ";
+        beverages_information_10.description = "Ingredients\n" +
+                "\n" +
+                "    1/2 gallon apple cider\n" +
+                "    12 oz rum (I used Captain Morgan Spiced Rum)\n" +
+                "    8 oz caramel ice cream topping\n" +
+                "\n" +
+                "Instructions\n" +
+                "\n" +
+                "    In 3 quart slow cooker combine ingredients, stir well.\n" +
+                "    Place on low heat setting for minimum of 3 hours. If your slow cooker has a \"warm\" setting you can switch to that after 3 hours or keep on \"low\".\n" +
+                "    Stir prior to ladling into mugs. \n" +
+                "\n" +
+                "Notes\n" +
+                "\n" +
+                "For a non-alcoholic version, omit rum." +
+                "\n" +
+                "\n\n";
+        beverages_information_10.color = Color.rgb(222, 122, 222);
+        beverages_information_10.image = "beverage_10";
+        beverages_information_10.URL = "http://whoneedsacape.com/2015/10/spiked-slow-cooker-caramel-apple-cider/";
+        items.add(beverages_information_10);
+
+
+
+        Information beverages_information_11 = new Information();
+        beverages_information_11.name = "Christmas Sangria  ";
+        beverages_information_11.description = "A light and crisp Christmas Sangria with fresh fruit and white wine.\n" +
+                "Ingredients\n" +
+                "\n" +
+                "    2 bottles of Mirassou Sauvignon Blanc\n" +
+                "    1 apple sliced into chunks\n" +
+                "    ½ pomegranate\n" +
+                "    1 - 2 oranges cut into slices\n" +
+                "    6 oz cranberries\n" +
+                "    2 can of 8oz cranberry lime seltzer\n" +
+                "    1 cup white grape juice\n" +
+                "    Sprigs of Rosemary {garnish}\n" +
+                "\n" +
+                "Instructions\n" +
+                "\n" +
+                "    Add the apple chunk, pomegranates, orange slices and cranberries to a pitcher.\n" +
+                "    Pour 2 bottles of Mirassou Sauvignon Blanc over the fresh fruit and refrigerate overnight or for at least 4-6 hours.\n" +
+                "    Just before serving add in cranberry lime seltzer and white grape juice.\n" +
+                "    Garnish each glass with a sprig of Rosemary and an orange slice.\n" +
+                "\n" +
+                "Serves: 4 - 8" +
+                "\n\n";
+        beverages_information_11.color = Color.rgb(222, 122, 222);
+        beverages_information_11.image = "beverage_11";
+        beverages_information_11.URL = "http://thenymelrosefamily.com/2014/11/christmas-sangria.html";
+        items.add(beverages_information_11);
+
+
+        return items;
+    }
+
+
+
+
+
+
+}
